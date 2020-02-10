5dea3c8f3818691debf7978862b2d017939cd098
Natashia White <whitenat@oregonstate.edu>
2020-02-10 11:16:07 -0800

    seperate cargo and rental classes

--- bike.rb
+++ bike.rb
@@ -7,33 +7,46 @@ class Bike
 
   attr_accessor :id, :color, :price, :weight, :rented, :cargo_contents
 
-  def initialize(id, color, price, weight = STANDARD_WEIGHT, rented = false)
+  def initialize(id, color, price, weight = STANDARD_WEIGHT)
     @id = id
     @color = color
     @price = price
     @weight = weight
-    @rented = rented
-    @cargo_contents = []
+    @cargo_contents = new.Cargo
   end
 
-  def rent!
-    self.rented = true
-  end
+end
 
-  def add_cargo(item)
-    self.cargo_contents << item
-  end
 
-  def remove_cargo(item)
-    self.cargo_contents.remove(item)
-  end
+class Cargo
 
-  def pannier_capacity
-    MAX_CARGO_ITEMS
-  end
+	def initialize()
+		@cargo_contents = []
+	end
 
-  def pannier_remaining_capacity
-    MAX_CARGO_ITEMS - self.cargo_contents.size
-  end
 
+	def add_cargo(item)
+	   self.cargo_contents << item
+	end
+
+	def remove_cargo(item)
+	   self.cargo_contents.remove(item)
+	end
+
+	def pannier_capacity
+	   MAX_CARGO_ITEMS
+	end
+
+	def pannier_remaining_capacity
+	   MAX_CARGO_ITEMS - self.cargo_contents.size
+	end
+end
+
+class Rental
+	def initialize(bike)
+		@bike = bike
+	end
+  	def rent!
+    	self.rented = true
+  	end
 end
